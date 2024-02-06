361f0c537f554b415822afc6d7714550fe204fa3
Sandeep Yadav <97525395+Sandeep00022@users.noreply.github.com>
2024-02-06 20:36:40 +0530

    implement sign up page ui

--- vite-project/src/pages/Signup.jsx
+++ vite-project/src/pages/Signup.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import { Button, Label, TextInput } from "flowbite-react";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="min-h-screen mt-20">
+      <div
+        className="flex p-3 max-w-3xl mx-auto 
+      flex-col md:flex-row md:items-center gap-5"
+      >
+        {/* left */}
+        <div className="flex-1">
+          <Link to="/" className="font-bold dark:text-white text-4xl">
+            <span
+              className="px-2 py-1 bg-gradient-to-r from-indigo-500 
+        via-purple-500 to-pink-500 rounded-lg text-white text-4xl "
+            >
+              Blog
+            </span>
+            Space
+          </Link>
+          <p className="text-sm mt-5">
+            Hey let's start with some good blogs. You can up with your email and
+            password or with Google
+          </p>
+        </div>
+        {/* right */}
 
-export default Signup
+        <div className="flex-1">
+          <form className="flex flex-col gap-4">
+            <div>
+              <Label value="Your username" />
+              <TextInput type="text" placeholder="Username" id="username" />
+            </div>
+            <div>
+              <Label value="Your email" />
+              <TextInput type="text" placeholder="name@email.com" id="email" />
+            </div>
+            <div>
+              <Label value="Your password" />
+              <TextInput type="text" placeholder="password" id="password" />
+            </div>
+            <Button gradientDuoTone={"purpleToPink"} type="submit">
+              Sign Up
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Have an account?</span>
+            <Link to="/sign-in" className="text-blue-500">
+              Sign In
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
