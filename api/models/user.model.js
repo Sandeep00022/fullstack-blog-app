e1a829980748f537b182bf48cb60db683b842064
Sandeep Yadav <97525395+Sandeep00022@users.noreply.github.com>
2024-02-06 18:57:27 +0530

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,27 @@
+import mongooose from "mongoose";
+
+const userSchmea = new mongooose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchmea);
+
+export default User;
