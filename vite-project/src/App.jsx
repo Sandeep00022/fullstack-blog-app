78948493bb20101dcb505ff4c6ae55df5f3ebc5e
Sandeep Yadav <97525395+Sandeep00022@users.noreply.github.com>
2024-02-07 21:48:28 +0530

    create and add and footer component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -7,6 +7,8 @@ import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import Signup from "./pages/Signup";
 import Header from "./components/Header";
+import { FooterCom } from "./components/Footer";
+
 
 const App = () => {
   return (
@@ -20,6 +22,7 @@ const App = () => {
         <Route path="dashboard" element={<Dashboard />} />
         <Route path="projects" element={<Projects />} />
       </Routes>
+      <FooterCom />
     </BrowserRouter>
   );
 };
