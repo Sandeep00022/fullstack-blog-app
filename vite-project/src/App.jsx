aa2b15d8175a41166317e9e1f618bd3d4b772032
Sandeep Yadav <97525395+Sandeep00022@users.noreply.github.com>
2024-02-08 21:38:00 +0530

    make dashboard private

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -8,7 +8,7 @@ import Projects from "./pages/Projects";
 import Signup from "./pages/Signup";
 import Header from "./components/Header";
 import { FooterCom } from "./components/Footer";
-
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
   return (
@@ -19,7 +19,9 @@ const App = () => {
         <Route path="about" element={<About />} />
         <Route path="/sign-in" element={<Signin />} />
         <Route path="/sign-up" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
         <Route path="/projects" element={<Projects />} />
       </Routes>
       <FooterCom />
