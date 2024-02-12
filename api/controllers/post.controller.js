4a90c6f57c6830bea60b61ecc203e943b078680b
Sandeep Yadav <97525395+Sandeep00022@users.noreply.github.com>
2024-02-12 20:02:37 +0530

    create get post api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -2,10 +2,10 @@ import Post from "../models/post.model.js";
 import { errorHandler } from "../utils/error.js";
 
 export const create = async (req, res, next) => {
-  console.log(req.body.isAdmin)
+  console.log(req.body.isAdmin);
   if (!req.user.isAdmin) {
     return next(errorHandler(403, "You are not allowed to create a post"));
-  } 
+  }
   if (!req.body.title || !req.body.content) {
     return next(errorHandler(400, "Please provide all request fields"));
   }
@@ -25,6 +25,52 @@ export const create = async (req, res, next) => {
   try {
     const savedPost = await newPost.save();
     res.status(201).json(savedPost);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getPosts = async (req,res,next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    const posts  = await Post.find({
+      ...(req.query.userId && { userId: re.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { category: req.query.category }),
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $option: "i" } },
+        ],
+      })
+  })
+      .sort({ updartedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+      const totlaPost = await Post.countDocuments();
+
+      const now  = new Date();
+
+      const oneMonthAgo = new Date(
+        now.getFullYear(),
+        now.getMonth() - 1,
+        now.getDate()
+      )
+
+      const lastMonthPosts = await Post.countDocuments({
+        createdAt: {$gte: oneMonthAgo}
+      })
+
+      res.status(200).json({
+        posts,
+        totlaPost,
+        lastMonthPosts,
+      })
+
   } catch (error) {
     next(error);
   }
