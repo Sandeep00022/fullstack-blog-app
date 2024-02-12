2c94c8205a354149046b6f443d0b095b43b3817b
Sandeep Yadav <97525395+Sandeep00022@users.noreply.github.com>
2024-02-12 19:13:45 +0530

    complete create post functionality

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -2,7 +2,8 @@ import Post from "../models/post.model.js";
 import { errorHandler } from "../utils/error.js";
 
 export const create = async (req, res, next) => {
-  if (!req.body.isAdmin) {
+  console.log(req.body.isAdmin)
+  if (!req.user.isAdmin) {
     return next(errorHandler(403, "You are not allowed to create a post"));
   } 
   if (!req.body.title || !req.body.content) {
