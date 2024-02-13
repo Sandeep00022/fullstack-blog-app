ac1bf299c3d4cbe46358b2a9fa2cf988a653a976
Sandeep Yadav <97525395+Sandeep00022@users.noreply.github.com>
2024-02-13 20:47:29 +0530

    create get users api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -66,16 +66,57 @@ export const deleteUser = async (req, res, next) => {
   }
 };
 
+export const signout = (req, res, next) => {
+  try {
+    res
+      .clearCookie("access_token")
+      .status(200)
+      .json("User has been signed out");
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const signout = (req, res, next ) => {
+export const getUsers = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "You are not allowed to see all users"));
+  }
 
   try {
-     res.clearCookie('access_token')
-     .status(200)
-     .json('User has been signed out');
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.sort === "asc" ? 1 : -1;
+
+    const users = await User.find()
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const usersWithoutPassword = users.map((user) => {
+      const { password, ...rest } = user._doc;
+      return rest;
+    });
+
+    const totalusers = await User.countDocuments();
+
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+    const lastMonthUsers = await User.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      users: usersWithoutPassword,
+      totalusers,
+      lastMonthUsers,
+    })
+
   } catch (error) {
-      next(error)
+    next(error);
   }
-}
-
-
+};
