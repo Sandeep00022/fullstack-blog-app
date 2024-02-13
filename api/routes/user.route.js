e1334259f343803e7b30937beb40e4905e7433d5
Sandeep Yadav <97525395+Sandeep00022@users.noreply.github.com>
2024-02-13 22:14:51 +0530

    show users to the admin dashboard

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -13,5 +13,5 @@ router.get("/test", test);
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signout);
-router.get("/getusers", verifyToken, getUsers);
+router.get("/user/getusers", verifyToken, getUsers);
 export default router;
--- vite-project/src/components/DashSidebar.jsx
+++ vite-project/src/components/DashSidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Sidebar } from "flowbite-react";
-import { HiUser, HiArrowSmRight, HiDocumentText } from "react-icons/hi";
+import { HiUser, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup } from "react-icons/hi";
 import { Link, useLocation } from "react-router-dom";
 import { signoutSuccess } from "../redux/user/userSlice";
 import { useDispatch, useSelector } from "react-redux";
@@ -56,6 +56,13 @@ export const DashSidebar = () => {
             <Link to="/dashboard?tab=posts">
               <Sidebar.Item active={tab === "posts"} icon={HiDocumentText}>
                 Posts
+              </Sidebar.Item>
+            </Link>
+          )}
+           {currentUser.isAdmin && (
+            <Link to="/dashboard?tab=users">
+              <Sidebar.Item active={tab === "users"} icon={HiOutlineUserGroup}>
+               Users
               </Sidebar.Item>
             </Link>
           )}
