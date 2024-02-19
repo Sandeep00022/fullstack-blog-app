764755b07f22fb4820744b3c4c3059e0e2683ec1
Sandeep Yadav <97525395+Sandeep00022@users.noreply.github.com>
2024-02-19 15:47:22 +0530

    fix: issue in path name

--- api/index.js
+++ api/index.js
@@ -32,7 +32,7 @@ app.use("/api/comment", commentRoutes);
 app.use(express.static(path.join(__dirname, "client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(pathy.join(__dirname, "vite-project", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "vite-project", "dist", "index.html"));
 });
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
