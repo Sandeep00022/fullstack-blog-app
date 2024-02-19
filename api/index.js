1a692d5d00ec78727ab40cf1b6b554106a6295be
Sandeep Yadav <97525395+Sandeep00022@users.noreply.github.com>
2024-02-19 15:57:09 +0530

    fix: dirname from client to vite-project

--- api/index.js
+++ api/index.js
@@ -29,7 +29,7 @@ app.use("/api", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
 
-app.use(express.static(path.join(__dirname, "client/dist")));
+app.use(express.static(path.join(__dirname, "/vite-project/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "vite-project", "dist", "index.html"));
